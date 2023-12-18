dc6d19d79fab80ef0d443b4d923308f4b8e27975
Agatha Lafaiety <agatha.lafaietyy@gmail.com>
2023-12-18 04:19:12 -0300

    feat: little things

--- first-script/main.tf
+++ first-script/main.tf
@@ -1,14 +1,27 @@
+terraform {
+  required_version = "1.6.6"
+
+  required_providers {
+    aws = {
+      source  = "haschicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "us-east-1"
-    profile = "tf014"
+  region  = "sa-east-1"
+  profile = "tf014"
 }
 
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-83764513410113"
+  bucket = "my-tf-test-bucket-alsv83764513410113"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-    Manageby = "Terraform"
+    ManageBy    = "Terraform"
+    Owner       = "Agatha Lafaiety"
+    UpdatedAAt  = "2023-12-18"
   }
 }
